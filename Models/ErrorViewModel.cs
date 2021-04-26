4eb6a4942418c7c76351c6bd4d62f00b30505450
AnetaCadova <acadova@redhat.com>
2021-04-26 02:44:36 +0200

    Initial commit - project created from template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace employee_training_tool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
