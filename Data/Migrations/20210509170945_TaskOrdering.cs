184c9a010552a7ef8ae72623d5267b75a971429f
AnetaCadova <acadova@redhat.com>
2021-05-10 02:05:06 +0200

    Task ordering created

---
+++ Data/Migrations/20210509170945_TaskOrdering.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace employee_training_tool.DataMigrations
+{
+    public partial class TaskOrdering : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "LearningPathTasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "AssignedTasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "LearningPathTasks");
+
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "AssignedTasks");
+        }
+    }
+}
---
+++ Data/Migrations/20210509223533_TaskOrdering-IgnoreProp.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace employee_training_tool.DataMigrations
+{
+    public partial class TaskOrderingIgnoreProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
